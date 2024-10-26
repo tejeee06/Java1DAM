1c74ea59aedf797fec072d4de9bb413250eaa348
tejeee06 <atejegom@gmail.com>
2024-10-26 15:42:34 +0200

    Update , sequencia d' escapament

---
+++ AEA1/AA/AA3/Sequencia.java
@@ -0,0 +1,9 @@
+package AEA1.AA.AA3;
+
+public class Sequencia {
+
+    public static void main(String[]args) {
+        System.out.println("\t" + "Fùtbol" + "\n" + "Club" + "\n" + "\t" + "Barcelona" + "\n" +"\"" + "1889" + "\"");
+    }
+
+}

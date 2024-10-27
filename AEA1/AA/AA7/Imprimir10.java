b9411e737149814eff4818b7462fb069ebdd21a5
tejeee06 <atejegom@gmail.com>
2024-10-27 18:50:06 +0100

    Update , Programa que imprimeix els numeros de l'1 al 10 utilitzant un for

---
+++ AEA1/AA/AA7/Imprimir10.java
@@ -0,0 +1,17 @@
+package AEA1.AA.AA7;
+
+public class Imprimir10 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Programa per imprimir els numeros de l 0 al 10.");
+
+        for (int i = 0; i <= 10; i++) {
+
+            System.out.println(i);
+        }
+
+        System.out.println("Fi del programa.");
+    }
+
+}

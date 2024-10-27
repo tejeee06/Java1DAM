5b7e3e43e4e96fd9badd059bf89ee5911a688acb
tejeee06 <atejegom@gmail.com>
2024-10-27 18:59:55 +0100

    Update , Triangle Nombre amb numero repetits , utilitzant for

---
+++ AEA1/PR/PR6/TriangleNombreRepetit.java
@@ -0,0 +1,26 @@
+package AEA1.PR.PR6;
+import java.util.Scanner;
+
+public class TriangleNombreRepetit {
+
+    public static void main(String[]args) {
+
+        Scanner lectorColumna = new Scanner(System.in);
+
+        System.out.println("Numero de columnes: ");
+        int columna = lectorColumna.nextInt();
+
+        for ( int i = 1; i <= columna; i++) {
+
+            for (int j = 1; j <= i; j++) {
+
+                System.out.print(i);
+            }
+
+            System.out.println("");
+
+        }
+
+    }
+
+}

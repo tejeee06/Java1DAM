eb634e2da04681dc2f910e12eab9a19d0656b543
tejeee06 <atejegom@gmail.com>
2024-11-29 17:45:46 +0100

    Exercici 4 de repas

---
+++ AEA2/AA/AA2/Exercici4.java
@@ -0,0 +1,34 @@
+package AEA2.AA.AA2;
+import java.util.*;
+
+public class Exercici4 {
+
+    public static void main(String[]args) {
+
+        int [] vector = {43, 76, 9, 1, 66, 32, 76, 2};
+
+        System.out.println("El vector inicial es : ");
+        System.out.println("");
+
+        for (int num : vector) {
+
+            System.out.println(num);
+
+        }
+
+        Arrays.sort(vector);
+
+        System.out.println("El vector endreçat es : ");
+        System.out.println("");
+
+
+        for (int num : vector) {
+
+            System.out.println(num);
+
+        }
+
+        System.out.println("");
+        System.out.println("El segon element mes petit del vector es " +vector[1]);
+    }
+}

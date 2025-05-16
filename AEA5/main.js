cd74cf3c39b10f6347d7914d20eadcc87da40a35
tejeee06 <atejegom@gmail.com>
2025-05-16 20:47:37 +0200

    docker-compose

--- AEA5/main.js
+++ AEA5/main.js
@@ -11,7 +11,7 @@ function createWindow() {
     },
   });
 
-  win.loadURL('http://localhost:3000'); // Cambiado a 3000
+  win.loadURL('http://localhost:5173'); // Puerto por defecto de Vite
 }
 
 app.whenReady().then(createWindow);

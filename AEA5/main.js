1a0d02c604ae53d6f4548556b8983d6cfd150cf6
tejeee06 <atejegom@gmail.com>
2025-05-16 21:42:10 +0200

    canvis

--- AEA5/main.js
+++ AEA5/main.js
@@ -1,29 +1,25 @@
 const { app, BrowserWindow } = require('electron');
-const path = require('path');
 
 function createWindow() {
   const win = new BrowserWindow({
     width: 1000,
     height: 900,
     webPreferences: {
-      nodeIntegration: true,
-      contextIsolation: false,
+      contextIsolation: true,
+      nodeIntegration: false,
     },
   });
 
-  win.loadURL('http://localhost:5173'); // Puerto por defecto de Vite
+  // Carga Vite en desarrollo
+  win.loadURL('http://localhost:5173');
 }
 
 app.whenReady().then(createWindow);
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+  if (process.platform !== 'darwin') app.quit();
 });
 
 app.on('activate', () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
 });
